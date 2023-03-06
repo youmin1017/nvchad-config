befdc911e59e1be79d5abc3e196a3311cf65f74b
youmin1017 <xx4016481@gmail.com>
2023-03-06 10:00:46 +0800

    update my configuations

---
+++ configs/overrides.lua
@@ -0,0 +1,76 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    disable = {
+      "python"
+    },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+local actions = require("telescope.actions")
+M.telescope = {
+  defaults = {
+    mappings = {
+      i = {
+        -- ["<Down>"] = actions.cycle_history_next,
+        -- ["<Up>"] = actions.cycle_history_prev,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+    }
+  }
+}
+
+local cmp = require("cmp")
+M.cmp = {
+  mapping = {
+    ["<C-p>"] = nil,
+    ["<C-n>"] = nil,
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+  }
+}
+
+return M
